c0c7763326d07ad60ca833ed0c919561639f5016
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2018-04-22 00:21:10 +0300

    remove unused imports

--- app/src/main/java/com/example/gay/kanji/data/KanjiRunnable.java
+++ app/src/main/java/com/example/gay/kanji/data/KanjiRunnable.java
@@ -1,10 +1,7 @@
 package com.example.gay.kanji.data;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
