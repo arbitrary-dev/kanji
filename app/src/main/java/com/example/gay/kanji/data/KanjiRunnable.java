4ef6d06b53fe7ffb2e290be5d8a30467d3e2b695
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2019-05-07 18:57:32 +0300

    rfct kanji lookup

--- app/src/main/java/com/example/gay/kanji/data/KanjiRunnable.java
+++ app/src/main/java/com/example/gay/kanji/data/KanjiRunnable.java
@@ -39,26 +39,15 @@ public class KanjiRunnable extends TaskRunnable {
     protected void runInner() throws InterruptedException {
         String gif = NO_DATA;
 
-        if (!isExtStorageAvailable())
+        if (!isExtStorageAvailable()) {
             Log.d(TAG, "External storage is not available");
-        else if (isExtStorageReadOnly())
+        } else if (isExtStorageReadOnly()) {
             Log.w(TAG, "External storage is read-only");
-        else if (!checkExtStoragePermissions())
+        } else if (!checkExtStoragePermissions()) {
             Log.e(TAG, "External storage permissions are not granted");
-        else {
+        } else {
             checkIfInterrupted();
-
-            File path = getExternalStoragePublicDirectory(DIRECTORY_PICTURES);
-            String appName = App.getName();
-            path = new File(path, appName);
-
-            checkIfInterrupted();
-
-            // TODO fallback to local storage
-            if (!path.exists() && path.mkdirs())
-                Log.d(TAG, "External storage was created at " + path.getAbsolutePath());
-
-            gif = prepareKanji(path, task.kanji);
+            gif = lookup(task.kanji);
         }
 
         checkIfInterrupted();
@@ -80,25 +69,49 @@ public class KanjiRunnable extends TaskRunnable {
             && ContextCompat.checkSelfPermission(context, WRITE_EXTERNAL_STORAGE) == PERMISSION_GRANTED;
     }
 
-    private String prepareKanji(File path, Character kanji) throws InterruptedException {
-        String filename = kanji + ".gif";
-        File file = new File(path, filename);
+    private String lookup(Character kanji) throws InterruptedException {
+        File path = getExternalStoragePublicDirectory(DIRECTORY_PICTURES);
+        String appName = App.getName();
+        path = new File(path, appName);
 
-        if (file.exists()) {
-            // TODO refactor cache quering to a separate TaskRunnable
-            String absPath = file.getAbsolutePath();
+        checkIfInterrupted();
+
+        // TODO fallback to local storage
+        if (!path.exists() && path.mkdirs())
+            Log.d(TAG, "External storage was created at " + path.getAbsolutePath());
+
+        File targetFile = new File(path, kanji + ".gif");
+        String result = lookupCached(targetFile);
+
+        if (result.equals(NO_DATA))
+            result = lookupDistroZip(targetFile);
+
+        // TODO fallback to downloading from WWWJDIC
+
+        return result;
+    }
+
+    private String lookupCached(File targetFile) {
+        if (targetFile.exists()) {
+            String absPath = targetFile.getAbsolutePath();
             logd("Found cached");
             return absPath;
         } else {
-            return unzip(filename, path);
+            return NO_DATA;
         }
     }
 
-    private String unzip(String filename, File path) throws InterruptedException {
-        if (!path.exists()) {
-            loge("No folder to store", "at", path.getAbsolutePath());
-            return NO_DATA;
+    private void checkParentDirectory(File targetFile) {
+        File parent = targetFile.getParentFile();
+        if (!parent.exists()) {
+            String path = parent.getAbsolutePath();
+            loge("No folder to store", "at", path);
+            throw new RuntimeException("Target path unavailable at " + path);
         }
+    }
+
+    private String lookupDistroZip(File targetFile) throws InterruptedException {
+        checkParentDirectory(targetFile);
 
         try {
             checkIfInterrupted();
@@ -108,24 +121,23 @@ public class KanjiRunnable extends TaskRunnable {
             try (ZipInputStream zis = new ZipInputStream(new BufferedInputStream(zipFile))) {
                 ZipEntry ze;
                 int count;
-                byte[] buffer = new byte[8192];
                 while ((ze = zis.getNextEntry()) != null) {
                     checkIfInterrupted();
 
-                    if (!ze.getName().equals(filename))
+                    if (!ze.getName().equals(targetFile.getName()))
                         continue;
 
                     logd("Found", "in distro zip");
 
-                    File file = new File(path, filename);
-                    try (FileOutputStream fout = new FileOutputStream(file)) {
+                    try (FileOutputStream fout = new FileOutputStream(targetFile)) {
+                        byte[] buffer = new byte[8192];
                         while ((count = zis.read(buffer)) != -1) {
                             checkIfInterrupted();
                             fout.write(buffer, 0, count);
                         }
                     }
 
-                    String absPath = file.getAbsolutePath();
+                    String absPath = targetFile.getAbsolutePath();
                     logd("ZipEntry", "was cached to", absPath);
 
                     return absPath;
@@ -135,7 +147,6 @@ public class KanjiRunnable extends TaskRunnable {
             throw new RuntimeException(e);
         }
 
-        // TODO fallback to downloading from WWWJDIC
         logd("No", "was found in distro zip");
 
         return NO_DATA;
