074b1ce5c72c7d963773e50b31c2075d769e12fa
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2020-06-06 20:03:04 +0300

    Use local storage for cached gifs

--- app/src/main/java/com/example/gay/kanji/data/KanjiRunnable.java
+++ app/src/main/java/com/example/gay/kanji/data/KanjiRunnable.java
@@ -1,9 +1,6 @@
 package com.example.gay.kanji.data;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.Environment;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.example.gay.kanji.App;
@@ -24,13 +21,6 @@ import java.nio.channels.ReadableByteChannel;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-import static android.content.pm.PackageManager.PERMISSION_GRANTED;
-import static android.os.Environment.DIRECTORY_PICTURES;
-import static android.os.Environment.MEDIA_MOUNTED;
-import static android.os.Environment.MEDIA_MOUNTED_READ_ONLY;
-import static android.os.Environment.getExternalStoragePublicDirectory;
 import static com.example.gay.kanji.data.Data.NO_DATA;
 
 public class KanjiRunnable extends TaskRunnable {
@@ -38,6 +28,7 @@ public class KanjiRunnable extends TaskRunnable {
     private static final String TAG = "KNJ";
 
     String getLoggingTag() { return TAG; }
+
     String getLoggingData() { return "gif"; }
 
     KanjiRunnable(DataTask task) {
@@ -48,50 +39,16 @@ public class KanjiRunnable extends TaskRunnable {
 
     @Override
     protected void runInner() throws InterruptedException {
-        String gif = NO_DATA;
-
-        if (!isExtStorageAvailable()) {
-            Log.d(TAG, "External storage is not available");
-        } else if (isExtStorageReadOnly()) {
-            Log.w(TAG, "External storage is read-only");
-        } else if (!checkExtStoragePermissions()) {
-            Log.e(TAG, "External storage permissions are not granted");
-        } else {
-            checkIfInterrupted();
-            gif = lookup(task.kanji);
-        }
-
         checkIfInterrupted();
-
+        String gif = lookup(task.kanji);
+        checkIfInterrupted();
         task.gif = gif;
     }
 
-    private static boolean isExtStorageAvailable() {
-        return MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
-    }
-
-    private static boolean isExtStorageReadOnly() {
-        return MEDIA_MOUNTED_READ_ONLY.equals(Environment.getExternalStorageState());
-    }
-
-    private boolean checkExtStoragePermissions() {
-        Context context = App.getContext();
-        return ContextCompat.checkSelfPermission(context, READ_EXTERNAL_STORAGE) == PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(context, WRITE_EXTERNAL_STORAGE) == PERMISSION_GRANTED;
-    }
-
     private String lookup(Character kanji) throws InterruptedException {
-        File path = getExternalStoragePublicDirectory(DIRECTORY_PICTURES);
-        String appName = App.getName();
-        path = new File(path, appName);
-
         checkIfInterrupted();
 
-        // TODO fallback to local storage
-        if (!path.exists() && path.mkdirs())
-            Log.d(TAG, "External storage was created at " + path.getAbsolutePath());
-
-        File targetFile = new File(path, kanji + ".gif");
+        File targetFile = new File(App.getContext().getCacheDir(), kanji + ".gif");
         String result = lookupCached(targetFile);
 
         if (result.equals(NO_DATA))
